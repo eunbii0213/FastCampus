39c327b542a048867daf926c9fcb204a932489c5
eunbii0213 <eunbii0213@naver.com>
2021-12-30 02:06:08 +0900

    practice07 실습코드 업로드입니다.

---
+++ src/chapter2/practice07/BirthDayTest.java
@@ -0,0 +1,16 @@
+package chapter2.practice07;
+
+public class BirthDayTest {
+
+	public static void main(String[] args) {
+		
+		Birthday date = new Birthday();
+		
+		date.setYear(2019);
+		date.setMonth(13);
+		date.setDay(3);
+		
+		date.showDate();
+	}
+
+}
