63831b8e633113f67b2a7e063cc432a10823c7ef
eunbii0213 <eunbii0213@naver.com>
2022-01-10 13:32:48 +0900

    practice13 실습코드입니다.

---
+++ src/chapter2/practice13/CompanyTest.java
@@ -0,0 +1,18 @@
+package chapter2.practice13;
+
+import java.util.Calendar;
+
+public class CompanyTest {
+
+	public static void main(String[] args) {
+		Company company = Company.getInstance();
+		Company company2 = Company.getInstance();
+		
+		System.out.println(company);
+		System.out.println(company2);
+		
+		Calendar calendar = Calendar.getInstance();
+
+	}
+
+}
